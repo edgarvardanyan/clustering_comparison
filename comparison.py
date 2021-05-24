dbb713dde3bb18220692bc236cd0ed5a18ee5107
edgar <edgarv@10web.io>
2021-05-24 09:45:49 +0400

    Comparing forel on the new data

--- comparison.py
+++ comparison.py
@@ -6,40 +6,44 @@ from sklearn.metrics import \
 import matplotlib.pyplot as plt
 from collections import defaultdict
 import pandas as pd
+from tqdm import tqdm
 
 from forel import ForelClustering
 
 pd.set_option('display.width', 300)
 pd.set_option('display.max_columns', 10)
 
-data = make_blobs(
-    n_samples=200,
-    n_features=2,
-    centers=16
-)
+# data = make_blobs(
+#     n_samples=1000,
+#     n_features=2,
+#     centers=10,
+#     random_state=10,
+# )
+data = np.array(pd.read_csv('/home/edgar/rau/clustering/data.csv'))
 
-plt.scatter(data[0][:, 0], data[0][:, 1])
-plt.show()
+# plt.scatter(data[:, 0], data[:, 1])
+# plt.show()
 
 models = []
 
-for i in range(2, 16):
-    models.append((KMeans(n_clusters=i), f"KMeans with k={i}"))
+# for i in range(2, 16):
+#     models.append((KMeans(n_clusters=i), f"KMeans with k={i}"))
 
-for i in np.linspace(1, 10, 18):
+for i in np.linspace(20, 400, 20):
     models.append((ForelClustering(r=i), f"Forel with r={i}"))
 
 results = defaultdict(list)
 
-for model, description in models:
-    predicted = model.fit_predict(data[0])
+for model, description in tqdm(models):
+    predicted = model.fit_predict(data)
     results['Model'].append(description)
     results['silhouette_score'].append(
-        silhouette_score(data[0], predicted, metric='euclidean'))
+        silhouette_score(data, predicted, metric='euclidean'))
     results['calinski_harabasz_score'].append(
-        calinski_harabasz_score(data[0], predicted))
+        calinski_harabasz_score(data, predicted))
     results['davies_bouldin_score'].append(
-        davies_bouldin_score(data[0], predicted))
+        davies_bouldin_score(data, predicted))
 
-results = pd.DataFrame(results)
-print(results)
+    df = pd.DataFrame(results)
+    print(df)
+    print("\n"*10)
